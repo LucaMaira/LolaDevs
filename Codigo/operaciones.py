b9437bf94a49d51eecb40b936c1c4c6afa544baf
Guille <gui0977@gmail.com>
2026-09-09 19:12:09 -0300

    hola

---
+++ Codigo/operaciones.py
@@ -0,0 +1,7 @@
+def matrizInicializador (matriz, filas, columnas, valor):
+    for f in range(filas):
+        matriz.append([])
+        for c in range(columnas):
+            matriz[f].append(valor)
+
+"Holaaa"
