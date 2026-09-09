1c32c2af143ee5bd489f157d3816d227a38291f6
Guille <gui0977@gmail.com>
2026-09-09 18:47:24 -0300

    modificacion main

--- Codigo/main.py
+++ Codigo/main.py
@@ -1,8 +1,9 @@
 def menu_principal():
+    print("-"*55)
+    print("\tBIENVENIDO AL PROGRAMA DE LOLLAPALOOZA")
+    print("-"*55)
 
-    print ("BIENVENIDO AL PROGRAMA DE LOLLAPALOOZA")
-
-    #menu:
+    #Menú:
     print ("1. Escenarios y capacidades.")
     print ("2. Actualizar concurrencia de un escenario y día.")
     print ("3. Buscar un escenario por código o nombre.")
@@ -14,9 +15,10 @@ def menu_principal():
     print ("9. Generar informes finales.")
     print ("10 SALIR.")
 
-    eleccion = input("Eliga una opcion (del 1 al 9 y 10 para EXIT): ")
+    eleccion = input("Eliga una opción (del 1 al 9 y 10 para EXIT): ")
     return eleccion
 
+#Ejecución del programa principal:
 if __name__ == "__main__":
     while True:
         opcion = menu_principal()
@@ -28,25 +30,25 @@ if __name__ == "__main__":
             print("3. Perry's Stage (Codigo: E3 - Capacidad: 30.000)")
             print("4. Alternative (Codigo: E4 - Capacidad: 25.000)")
         elif opcion == "2":
-            print("Actualizacion manual de concurrencia:")
-            print("Seleccione escenario (E1, E2, E3, E4):")
-            print("Seleccione dia (Viernes, Sabado, Domingo):")
-            print("Ingrese nueva cantidad de asistentes:")
+            print("Actualización manual de concurrencia: ")
+            print("Seleccione escenario (E1, E2, E3, E4): ")
+            print("Seleccione dia (Viernes, Sabado, Domingo): ")
+            print("Ingrese nueva cantidad de asistentes: ")
         elif opcion == "3":
-            print("Busqueda puntual de escenario:")
-            print("Ingrese codigo unico (ej. E1) o nombre del escenario:")
+            print("Busqueda puntual de escenario: ")
+            print("Ingrese codigo unico (ej. E1) o nombre del escenario: ")
         elif opcion == "4":
-            print("Consulta de concurrencia por dia:")
-            print("Seleccione el dia a consultar (Viernes, Sabado, Domingo):")
+            print("Consulta de concurrencia por dia: ")
+            print("Seleccione el dia a consultar (Viernes, Sabado, Domingo): ")
         elif opcion == "5":
-            print("Registro de entradas vendidas:")
-            print("Seleccione categoria de acceso (Campo General, VIP Lounge, LollaFam, Prensa):")
-            print("Seleccione dia (Viernes, Sabado, Domingo):")
-            print("Ingrese cantidad de entradas vendidas a registrar:")
+            print("Registro de entradas vendidas: ")
+            print("Seleccione categoria de acceso (Campo General, VIP Lounge, LollaFam, Prensa): ")
+            print("Seleccione dia (Viernes, Sabado, Domingo): ")
+            print("Ingrese cantidad de entradas vendidas a registrar: ")
         elif opcion == "6":
-            print("Consultar entradas vendidas y disponibles por categoria")
+            print("Consultar entradas vendidas y disponibles por categoría")
         elif opcion == "7":
-            print("Consultar estadisticas e indicadores")
+            print("Consultar estadísticas e indicadores")
         elif opcion == "8":
             print("Consultar ranking y Top 3 de escenarios")
         elif opcion == "9":
@@ -55,7 +57,7 @@ if __name__ == "__main__":
             print("Gracias por utilizar el sistema de gestion de Lollapalooza.")
             break
         else:
-            print("Opcion invalida. Debe ingresar un numero del 1 al 10.")
+            print("Opcion invalida, debe ingresar un número del 1 al 10.")
 
     
 
